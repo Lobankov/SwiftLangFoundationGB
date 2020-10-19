702f56550fac1ab8f7a628829c8f403143d03b97
Alexey <alexey.lobankov@bellerage.ru>
2020-10-19 10:02:22 +0300

    Second lesson

--- GB/SwiftFoundationCourse.playground/Contents.swift
+++ GB/SwiftFoundationCourse.playground/Contents.swift
@@ -1,108 +1,120 @@
 import Foundation
 
-//------------ 1 ---------------//
+//------------ 1 ------------//
 
-/// Solves a quadratic equation (like ax^2 + bx + c = 0)
-/// - Parameters:
-///   - a: quadratic coefficient
-///   - b: linear coefficient
-///   - c: constant
-/// - Returns: a set of roots of passed equation (maximum 2) or nil if there are no roots
-/// - Remark: 'a' should be non zero, otherwise it's a linear equation
-func solveQuadraticEquation(quadratic a: Float, linear b: Float, constant c: Float) -> Set<Float>? {
+/// Check if the given number is even (0, 2, 4, 6 etc.)
+/// - Parameter number: number to check
+/// - Returns: True if even, false if odd
+func isEven(_ number: Int) -> Bool {
+    return number % 2 == 0
+}
+
+let myNumber = 3
+let myNumberIsEven = isEven(myNumber)
+print(myNumberIsEven)
+
+
+//------------ 2 ------------//
+
+/// Check if the given number divided by 3 entirely
+/// - Parameter number: number to check
+/// - Returns: True if divided entirely, otherwise false
+func isDividedByThreeEntirely(_ number: Int) -> Bool {
+    return number % 3 == 0
+}
+
+let secondNumber = 0
+let secondNumberDividesByThree = isDividedByThreeEntirely(secondNumber)
+print(secondNumberDividesByThree)
+
+
+//------------ 3 ------------//
+
+/// Creates an array of integers
+/// - Parameter length: array length
+/// - Returns: array of numbers
+func createArrayWith(length: Int) -> [Int] {
+    guard length > 0 else {
+        fatalError("Upper bound must be grater than 0")
+    }
+    var array: [Int] = []
+    for i in 1...length {
+        array.append(i)
+    }
+    return array
+}
+
+let array = createArrayWith(length: 100)
+
+
+//------------ 4 ------------//
+
+// Even numbers clean
+
+/// Removes all even numbers from array
+/// - Parameter array: original array
+/// - Returns: cleaned array
+func removeAllEvenNumbersFrom(_ array: [Int]) -> [Int] {
     
-    guard a != 0 else {
-        // we should throw here I guess :)
-        
-        
-        // bx = -c
-        let linearX = -c / b
-        return [linearX]
+    var cleanedArray = array
+    cleanedArray.removeAll(where: { $0 % 2 == 0 })
+    
+    return cleanedArray
+}
+
+/// Removes all even numbers from array (bad and inefficient implementation)
+/// - Parameter array: original array
+/// - Returns: cleaned array
+/// - Remark: we need to iterate backwards, otherwise we'll get index out of range exception
+func dummyReverseLoopClean(of array: [Int]) -> [Int] {
+    
+    // create a copy of array, so we can modify it
+    var cleanedArray = array
+    
+    // iterate through the array copy backwards
+    for i in (0..<cleanedArray.count).reversed() {
+        // if a current number is even...
+        if cleanedArray[i] % 2 == 0 {
+            // remove it
+            cleanedArray.remove(at: i)
+        }
+    }
+    return cleanedArray
+}
+
+// first approach
+let noEvenNumbers1 = removeAllEvenNumbersFrom(array)
+
+// second approach
+let noEvenNumbers2 = array.filter { $0 % 2 != 0 }
+
+// third approach
+let noEvenNumbers3 = dummyReverseLoopClean(of: array)
+
+
+// Remove all that are not divided by 3
+
+// You get the point, I know how to use functions and stuff. Only filter this time :D
+let cleanedArray = noEvenNumbers1.filter { $0 % 3 != 0}
+print(cleanedArray)
+
+
+//------------ 5 ------------//
+
+func getFibonacciArray(of length: Int) -> [UInt] {
+    
+    var fibonacciArray : [UInt] = [1, 1]
+    
+    guard length > 1 else {
+        return [1]
     }
     
-    // D = b^2 - 4ac
-    let discriminant = b * b - 4 * a * c
-    
-    // if discriminant is < 0, we have no roots
-    guard discriminant >= 0 else {
-        return nil
+    (2..<length).forEach { i in
+        fibonacciArray.append(fibonacciArray[i-1] + fibonacciArray[i - 2])
     }
     
-    // first root
-    let x1 = (-b + sqrt(discriminant)) / 2 * a
-    
-    // second root
-    let x2 = (-b - sqrt(discriminant)) / 2 * a
-    
-    // set is used just to avoid same roots. Because if discriminant is 0, both x1 and x2 are the same
-    let roots : Set<Float> = [x1, x2]
-    
-    return roots
+    return fibonacciArray
 }
 
-//  x^2 - 8x + 12 = 0 --- 2 roots
-// 5x^2 + 3x + 7  = 0 --- 0 root
-//  x^2 - 6x + 9  = 0 --- 1 root
-if let roots = solveQuadraticEquation(quadratic: -1, linear: -2, constant: 15) {
-    print("Roots: \(roots)")
-} else {
-    print("Discriminant is less than zero, no roots for u :)")
-}
-
-
-
-//------------ 2 ---------------//
-
-/// Calculates the area of a right triangle
-/// - Parameters:
-///   - cathetusA: first cathetus
-///   - cathetusB: second cathetus
-/// - Returns: area of a given triangle
-func getRightTriangleArea(cathetusA: Float, cathetusB: Float) -> Float {
-    return cathetusA * cathetusB / 2
-}
-
-/// Calculates the hypotenuse of a right triangle
-/// - Parameters:
-///   - cathetusA: first cathetus
-///   - cathetusB: second cathetus
-/// - Returns: hypotenuse of a given triangle
-func getRightTriangleHypotenuse(cathetusA: Float, cathetusB: Float) -> Float {
-    return sqrt(cathetusA * cathetusA + cathetusB * cathetusB)
-}
-
-/// Calculates the perimeter of a right triangle
-/// - Parameters:
-///   - cathetusA: first cathetus
-///   - cathetusB: second cathetus
-/// - Returns: perimeter of a given triangle
-func getRightTrianglePerimeter(cathetusA: Float, cathetusB: Float) -> Float {
-    return cathetusA + cathetusB + getRightTriangleHypotenuse(cathetusA: cathetusA, cathetusB: cathetusB)
-}
-
-let a: Float = 3.0
-let b: Float = 4.0
-
-print("Hypotenuse: \(getRightTriangleHypotenuse(cathetusA: a, cathetusB: b))")
-print("Area: \(getRightTriangleArea(cathetusA: a, cathetusB: b))")
-print("Perimeter: \(getRightTrianglePerimeter(cathetusA: a, cathetusB: b))")
-
-
-//------------ 3 ---------------//
-
-/// Wanna be rich bruh?
-/// - Parameters:
-///   - amount: your 10 bucks
-///   - rate: use 0.05 for 5%, not 5 ಠ_ಠ
-///   - years: positive infinity
-/// - Returns: ( ͡° ͜ʖ ͡°)
-func calculateDummyBankDeposit(money amount: Double, deposit rate: Double, in years: UInt) -> Double {
-    
-    // check for positive values should be here...
-    
-    let extraPercentMoney = amount * rate * Double(years)
-    
-    return amount + extraPercentMoney
-}
-
-print(calculateDummyBankDeposit(money: 100, deposit: 0.05, in: 5))
+// UInt cannot hold fibonacci number greater than 92th
+let arrayFib = getFibonacciArray(of: 92)
